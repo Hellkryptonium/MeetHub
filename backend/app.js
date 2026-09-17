ebc92733a94d76891facb8119ce3fa4038b86e74
hellkryptonium <harishjs1006@gmail.com>
2026-09-18 02:15:10 +0530

    Repository Setup

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+import express, { urlencoded } from "express";
+import {createServer} from "node:http";
+
+import {Server} from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./src/controllers/socketManager.js";
+
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({ limit: "40kb", extended: true}));
+
+app.get("/home", (req, res) => {
+    return res.json({ "hello": "World"});
+});
+
+const start = async() => {
+    const connectionDb = await mongoose.connect("mongodb+srv://harishjs1006_db_user:giPsEFIo1TmziEsy@cluster0.mffwpou.mongodb.net/")
+    
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("Listening on port 8000");
+    });
+}
+
+start();
