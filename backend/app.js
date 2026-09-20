e5bb5d50038b42b49dd8184f2978c65faf3fa285
hellkryptonium <harishjs1006@gmail.com>
2026-09-20 21:59:21 +0530

    env integration

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+app.set("port", (process.env.PORT  || 8000));
 
-app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({ limit: "40kb", extended: true}));
